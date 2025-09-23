c4638041195a723ff0bb67e85df563be0661c4fd
Oleksandr Mazur <a9p9m9@gmail.com>
2025-09-23 18:47:02 +0300

    register routes for Flight, Route, Airplane, Crew, Airport, AirplaneType, and Order viewsets in flight/urls.py

--- flight/urls.py
+++ flight/urls.py
@@ -1,4 +1,28 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from flight.views import (
+    CrewViewSet,
+    RouteViewSet,
+    AirportViewSet,
+    AirplaneTypeViewSet,
+    AirplaneViewSet,
+    FlightViewSet,
+    OrderViewSet
+)
+
+
 app_name = "flight"
 
+router = routers.DefaultRouter()
 
-urlpatterns = []
+router.register("crews", CrewViewSet, basename="crew")
+router.register("routes", RouteViewSet, basename="route")
+router.register("airports", AirportViewSet, basename="airport")
+router.register("airplane-types", AirplaneTypeViewSet, basename="airplane-type")
+router.register("airplanes", AirplaneViewSet, basename="airplane")
+router.register("flights", FlightViewSet, basename="flight")
+router.register("orders", OrderViewSet, basename="order")
+
+
+urlpatterns = [path("", include(router.urls))]
