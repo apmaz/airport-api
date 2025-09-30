82aff69d269e7753bb8129998dd2a9df41cc09b7
Oleksandr Mazur <a9p9m9@gmail.com>
2025-09-30 14:02:27 +0300

    add fixtures/airport_api_db_data.json to project

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -177,3 +178,8 @@ SPECTACULAR_SETTINGS = {
         "defaultModelExpandDepth": 2,
     }
 }
+
+
+FIXTURE_DIRS = [
+    os.path.join(BASE_DIR, "fixtures"),
+]
